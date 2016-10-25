c4e02c9d60063a3940bf8cfdea443db2b22bec18
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2016-10-25 13:24:21 -0400

    :space_invader: collapsed menu with show button

--- proto/js/header.js
+++ proto/js/header.js
@@ -4,27 +4,34 @@ function init() {
   $("#main-search input[type=radio]").click(function() {
     updateSearchRadio($(this).val())
   })
+
   $("#search-query-text").focus(function() {
     showSearch()
   })
+
   $("#main-search input[type=radio]").focus(function() {
     focusRadio($(this).val())
   })
+
   $("#main-search input[type=radio]").blur(function() {
     blurRadio($(this).val())
   })
+
   $("#search-query-text, .search-type, .search-button, .search-radio-label").on("focus click",  function() {
     if (hideTimeout) clearTimeout(hideTimeout)
     showSearch()
   })
+
   $("#search-query-text, .search-type, .search-button, .search-radio-label").blur(function() {
     hideTimeout = setTimeout(hideSearch, 100)
   })
+
   $(".email-toggle a").click(function(e) {
     if ($(e.target).closest('.email-toggle').length && !$(e.target).parents("#email-updates").length) {
       toggleEmail()
     }
   })
+
   $("body").on("click touchstart touchend", function(e) {
     if (!$(e.target).closest('.email-toggle').length && !$(e.target).parents("#email-updates").length) {
       hideEmail()
@@ -36,11 +43,13 @@ function init() {
       hideTimeout = setTimeout(hideSearch, 100)
     }
   })
+
   $(".login-toggle a").click(function(e) {
     if ($(e.target).closest('.login-toggle').length && !$(e.target).parents("#login-links").length) {
       toggleLogin()
     }
   })
+
   $("input[name=available]").change(function() {
     var value = $("input[name=available]:checked").val()
     if (value == "library") {
@@ -50,9 +59,14 @@ function init() {
     }
   })
 
-  $(".menu.main-menu-toggle").click(function() {
+  $(".main-menu-toggle").click(function() {
     $(this).remove()
     $(".nav-buttons, .service-buttons").css("display","block")
+  })
+
+  $(".collapsed-menu-toggle").click(function() {
+    $(this).remove()
+    $("header").removeClass("collapsed")
   })
 }
 
